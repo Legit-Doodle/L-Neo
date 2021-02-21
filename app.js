f6be9aecb582a0b1ccb099dee286c3a267578c72
Mayank Raj <mayank42293@gmail.com>
2021-02-21 20:27:56 +0530

    added a link one of my sites

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 //javascript
+function handleClick(){
+    //alert("clicked");
+    var url = "https://damp-reef-03995.herokuapp.com/";
+    window.location=url;
+}
+document.querySelector(".kmare").addEventListener("click",handleClick);
